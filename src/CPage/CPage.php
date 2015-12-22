d4359231caba7f2e21ecff1ed24fc70b77e44a8d
Fredrik Nilsson <fn@live.se>
2015-12-22 11:31:53 +0000

    Created CPage class to handle Page html-generation, refactored page.php.

---
+++ src/CPage/CPage.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Show Page
+ * Create html-output to render a page from Loom CMS
+ */
+class CPage
+{
+
+    public static function Get($url, $db)
+    {
+        $content = new CContent($db);
+        $filter = new CTextFilter();
+
+        $page = $content->GetPage($url);
+        $title = htmlentities($page->title, null, 'UTF-8');
+        $loom['title'] = $title;
+        $data = htmlentities($page->DATA, null, 'UTF-8');
+        $data = $filter->doFilter($data, $page->FILTER);
+        $editLink = "<a href=\"edit.php?id={$page->id}\">Redigera</a>";
+        $meta = "Publicerat: {$page->published} " . $editLink;
+
+        $out = <<<EOD
+        <article class="page">
+            <header>
+                <h1>{$title}</h1>
+            </header>
+            {$data}
+            <footer class="page-meta">{$meta}</footer>
+        </article>
+EOD;
+        return $out;
+    }
+}
