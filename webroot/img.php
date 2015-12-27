8e08f26fea04c78db3adedf78243421f578c622c
Fredrik Nilsson <fn@live.se>
2015-12-27 10:21:51 +0000

    v3

--- webroot/img.php
+++ webroot/img.php
@@ -41,13 +41,47 @@ function verbose($message) {
 
 
 
+/**
+ * Output an image together with last modified header.
+ *
+ * @param string $file as path to the image.
+ * @param boolean $verbose if verbose mode is on or off.
+ */
+function outputImage($file, $verbose) {
+  $info = getimagesize($file);
+  !empty($info) or errorMessage("The file doesn't seem to be an image.");
+  $mime   = $info['mime'];
+
+  $lastModified = filemtime($file);
+  $gmdate = gmdate("D, d M Y H:i:s", $lastModified);
+
+  if($verbose) {
+    verbose("Memory peak: " . round(memory_get_peak_usage() /1024/1024) . "M");
+    verbose("Memory limit: " . ini_get('memory_limit'));
+    verbose("Time is {$gmdate} GMT.");
+  }
+
+  if(!$verbose) header('Last-Modified: ' . $gmdate . ' GMT');
+  if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $lastModified){
+    if($verbose) { verbose("Would send header 304 Not Modified, but its verbose mode."); var_dump($_SERVER['HTTP_IF_MODIFIED_SINCE']);exit; }
+    header('HTTP/1.0 304 Not Modified');
+  } else {
+    if($verbose) { verbose("Would send header to deliver image with modified time: {$gmdate} GMT, but its verbose mode."); var_dump($_SERVER['HTTP_IF_MODIFIED_SINCE']); exit; }
+    header('Content-type: ' . $mime);
+    readfile($file);
+  }
+  exit;
+}
+
+
 //
 // Get the incoming arguments
 //
-$src      = isset($_GET['src'])     ? $_GET['src']      : null;
-$verbose  = isset($_GET['verbose']) ? true              : null;
-$saveAs   = isset($_GET['save-as']) ? $_GET['save-as']  : null;
-$quality  = isset($_GET['quality']) ? $_GET['quality']  : 60;
+$src      = isset($_GET['src'])      ? $_GET['src']      : null;
+$verbose  = isset($_GET['verbose'])  ? true              : null;
+$saveAs   = isset($_GET['save-as'])  ? $_GET['save-as']  : null;
+$quality  = isset($_GET['quality'])  ? $_GET['quality']  : 60;
+$ignoreCache = isset($_GET['no-cache']) ? true           : null;
 
 $pathToImage = realpath(IMG_PATH . $src);
 
@@ -105,22 +139,37 @@ if($verbose) {
 }
 
 
+
 //
 // Creating a filename for the cache
 //
-$parts      = pathinfo($pathToImage);
+$parts          = pathinfo($pathToImage);
 $fileExtension  = $parts['extension'];
-$saveAs     = is_null($saveAs) ? $fileExtension : $saveAs;
-$quality_   = is_null($quality) ? null : "_q{$quality}";
-$dirName    = preg_replace('/\//', '-', dirname($src));
+$saveAs         = is_null($saveAs) ? $fileExtension : $saveAs;
+$quality_       = is_null($quality) ? null : "_q{$quality}";
+$dirName        = preg_replace('/\//', '-', dirname($src));
 $cacheFileName = CACHE_PATH . "-{$dirName}-{$parts['filename']}_{$width}_{$height}{$quality_}.{$saveAs}";
 $cacheFileName = preg_replace('/^a-zA-Z0-9\.-_/', '', $cacheFileName);
 
 if($verbose) { verbose("Cache file is: {$cacheFileName}"); }
 
 
+//
+// Is there already a valid image in the cache directory, then use it and exit
+//
+$imageModifiedTime = filemtime($pathToImage);
+$cacheModifiedTime = is_file($cacheFileName) ? filemtime($cacheFileName) : null;
 
-// var_dump($parts);
+// If cached image is valid, output it.
+if(!$ignoreCache && is_file($cacheFileName) && $imageModifiedTime < $cacheModifiedTime) {
+  if($verbose) { verbose("Cache file is valid, output it."); }
+  outputImage($cacheFileName, $verbose);
+}
+
+if($verbose) { verbose("Cache is not valid, process image and create a cached version of it."); }
+
+
+
 //
 // Open up the image from file
 //
@@ -175,14 +224,4 @@ if($verbose) {
 //
 // Output the resulting image
 //
-if($verbose) {
-  verbose("Memory peak: " . round(memory_get_peak_usage() /1024/1024) . "M");
-  verbose("Memory limit: " . ini_get('memory_limit'));
-}
-else {
-  $info = getimagesize($cacheFileName);
-  !empty($info) or errorMessage("The file doesn't seem to be an image.");
-  $mime = $info['mime'];
-  header('Content-type: ' . $mime);
-  readfile($cacheFileName);
-}
+outputImage($cacheFileName, $verbose);
